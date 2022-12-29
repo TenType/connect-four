394c1e9433493e71c4a868e8471b13cb3d1b7903
TenType <55125103+TenType@users.noreply.github.com>
2022-12-29 15:00:15 -0800

    Alpha-beta pruning

--- src/solver.rs
+++ src/solver.rs
@@ -3,10 +3,14 @@ use crate::{Game, HEIGHT, WIDTH};
 pub type Score = i8;
 
 pub fn solve(game: Game) -> Score {
-    negamax(game)
+    negamax(
+        game,
+        -((WIDTH * HEIGHT / 2) as Score),
+        (WIDTH * HEIGHT / 2) as Score,
+    )
 }
 
-fn negamax(game: Game) -> Score {
+fn negamax(game: Game, mut alpha: Score, mut beta: Score) -> Score {
     if game.is_draw() {
         return 0;
     }
@@ -17,18 +21,28 @@ fn negamax(game: Game) -> Score {
         }
     }
 
-    let mut best_score = -((WIDTH * HEIGHT) as Score);
+    let max = ((WIDTH * HEIGHT - 1 - game.moves()) / 2) as Score;
+
+    if beta > max {
+        beta = max;
+        if alpha >= beta {
+            return beta;
+        }
+    }
 
     for col in 0..WIDTH {
         if game.is_unfilled(col) {
             let mut new_game = game.clone();
             new_game.unchecked_play(col);
-            let score = -negamax(new_game);
-            if score > best_score {
-                best_score = score;
+            let score = -negamax(new_game, -beta, -alpha);
+            if score >= beta {
+                return score;
+            }
+            if score > alpha {
+                alpha = score;
             }
         }
     }
 
-    best_score
+    alpha
 }
