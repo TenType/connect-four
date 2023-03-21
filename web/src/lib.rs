9af9ae7123b7f61120369804ac9a691ed010b1ac
TenType <55125103+TenType@users.noreply.github.com>
2023-03-20 19:01:52 -0700

    Add basic web UI

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,18 +1,62 @@
-mod utils;
-
+use connect_four_engine::{Game, Player, Status, HEIGHT};
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
 
 #[wasm_bindgen(start)]
 pub fn main() {
-    utils::set_panic_hook();
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
 }
 
-#[wasm_bindgen]
-pub fn hello(name: &str) {
-    alert(&format!("Hello, {name}!"));
+#[wasm_bindgen(js_name = Game)]
+pub struct GameWrapper(Game);
+
+impl Default for GameWrapper {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[wasm_bindgen(js_class = Game)]
+impl GameWrapper {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> GameWrapper {
+        GameWrapper(Game::new())
+    }
+
+    pub fn can_play(&self, col: usize) -> bool {
+        self.0.can_play(col).is_ok()
+    }
+
+    pub fn play(&mut self, col: usize) {
+        self.0.play(col).expect("column should be valid")
+    }
+
+    pub fn is_game_over(&self) -> bool {
+        self.0.is_game_over()
+    }
+
+    pub fn available_row(&self, col: usize) -> usize {
+        for row in 0..HEIGHT {
+            if self.0.at(col, row).is_none() {
+                return row;
+            }
+        }
+        panic!("no pieces in column {col}")
+    }
+
+    pub fn first_player_turn(&self) -> bool {
+        self.0.turn() == Player::P1
+    }
+
+    pub fn winner(&self) -> u8 {
+        match self.0.winner() {
+            None => 0,
+            Some(Player::P1) => 1,
+            Some(Player::P2) => 2,
+        }
+    }
+
+    pub fn is_draw(&self) -> bool {
+        self.0.status() == Status::Draw
+    }
 }
--- web/www/index.js
+++ web/www/index.js
@@ -1,5 +1,73 @@
-import init, { hello } from '../pkg/connect_four_website.js';
+import init, { Game } from '../pkg/connect_four_website.js';
+
+const board = document.getElementById('board');
+const WIDTH = 7;
+const HEIGHT = 6;
 
 init().then(() => {
-    hello('Rust + WebAssembly');
+  const game = new Game();
+  setup(game);
 });
+
+/**
+ * @param {Game} game
+ */
+function setup(game) {
+  for (let row = 0; row < HEIGHT; row++) {
+    for (let col = 0; col < WIDTH; col++) {
+      const tile = document.createElement('div');
+      tile.classList.add('tile');
+      board.appendChild(tile);
+      tile.onclick = () => makeMove(game, col);
+    }
+  }
+}
+
+/**
+ * @param {Game} game
+ * @param {number} col
+ */
+function makeMove(game, col) {
+  if (game.is_game_over() || !game.can_play(col)) return;
+
+  const piece = document.createElement('div');
+  piece.classList.add('piece');
+
+  if (game.first_player_turn()) {
+    piece.dataset.color = 'red';
+  } else {
+    piece.dataset.color = 'yellow';
+  }
+
+  const row = HEIGHT - game.available_row(col) - 1;
+
+  game.play(col);
+
+  const index = row * 7 + col;
+  const gridCell = board.children[index];
+  gridCell.appendChild(piece);
+
+  const topY = board.children[0].getBoundingClientRect().y;
+  const pieceY = piece.getBoundingClientRect().y;
+  const distance = topY - pieceY - 60;
+
+  const animation = piece.animate(
+    {
+      transform: [
+        `translateY(${distance}px)`,
+        'translateY(0px)',
+        `translateY(${distance / 10}px)`,
+      ],
+      offset: [0, 0.5, 0.7],
+      easing: ['cubic-bezier(0.22, 0, 0.42, 0)', 'ease', 'ease'],
+    },
+    500
+  );
+
+  const winner = game.winner();
+  if (winner != 0) {
+    animation.onfinish = () => alert(`Player ${winner} won!`);
+  } else if (game.is_draw()) {
+    animation.onfinish = () => alert('The game is a draw!');
+  }
+}
